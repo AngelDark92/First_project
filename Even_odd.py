3ae8b6df2f14a65cd5c21ea74d85977b95008880
AngelDark92 <53372897+AngelDark92@users.noreply.github.com>
2019-09-28 15:56:05 +0100

    Update Even_odd.py

--- Even_odd.py
+++ Even_odd.py
@@ -1,25 +1,39 @@
-while 1>0:
+i=0
+while i!=2:
+	i=0
 	num=int(input("Input a number to check if even or odd: "))
 	check=int(input("Input another number: "))
 	if num%4==0:
-		print("the number", num," is a multiple of 4")
+		print(f"the number {num} is a multiple of 4")
 	else:
-		print("the number", num," is no multiple of 4")
+		print(f"the number {num} is no multiple of 4")
 	if num%2==0:
-		print("the number", num," is even")
+		print(f"the number {num} is even")
 	else:
-		print("the number", num," is odd and no multiple of 4")
-		loppo=input("continue yes or no? ")
-		if loppo=="no":
-			break
+		print(f"the number {num} is odd")
 	if num%check==0:
-		print(num, " and ",check, " divide evenly into each other")
-		loppo=input("continue yes or no? ")
-		if loppo=="no":
-			break
+		print(f"{num} and {check} divide evenly into each other")
+		while i==0:
+			loppo=input("continue yes or no? ")
+			if loppo=="yes":
+				i=1
+				continue
+			elif loppo=="no":
+				i=2
+				break
+			else:
+				print("your reply is not valid, re enter")
+				continue	
 	else:
-		print(num, " and ",check," do not divide evenly into each other")
-		loppo=input("continue yes or no? ")
-		if loppo=="no":
-			break
-	
+		print(f"{num} and {check} do not divide evenly into each other")
+		while i==0:
+			loppo=input("continue yes or no? ")
+			if loppo=="yes":
+				i=1
+				continue
+			elif loppo=="no":
+				i=2
+				break
+			else:
+				print("your reply is not valid, re enter")
+				continue
